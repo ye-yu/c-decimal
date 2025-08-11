86482f4629f776ec58acea9195caded8cbd07a40
raflie <rafolwen98@gmail.com>
2025-08-11 14:43:06 +0800

    div_10: remove b_dec divide by 10 (due to precision, to be fixed)

--- include/b_dec_div_10.h
+++ include/b_dec_div_10.h
@@ -4,7 +4,7 @@
 // i got it from here lol https://math.stackexchange.com/a/3001889/704406
 int div_10(const uint64_t num, uint64_t *quotient, uint64_t *remainder);
 int div_10_uint128(const uint128_t num, uint128_t *quotient, uint128_t *remainder);
-
-int div_10_b_dec(const b_dec num, b_dec *result, uint8_t *remainder);
-
+int div_10_b_uint_arr_no_malloc(const b_uint *num, b_uint *result, uint8_t *remainder,
+                                b_uint *shift_buffer_left, b_uint *shift_buffer_right, b_uint *b_uint_one,
+                                const size_t size);
 int div_10_b_uint_arr(const b_uint *num, b_uint *result, uint8_t *remainder, const size_t size);
--- src/div_10.c
+++ src/div_10.c
@@ -53,53 +53,14 @@ int div_10_uint128(const uint128_t num, uint128_t *quotient, uint128_t *remainde
     return 0;
 }
 
-int div_10_b_uint_arr(const b_uint *num, b_uint *result, uint8_t *remainder, const size_t size)
+int div_10_b_uint_arr_no_malloc(const b_uint *num, b_uint *result, uint8_t *remainder,
+                                b_uint *shift_buffer_left, b_uint *shift_buffer_right, b_uint *b_uint_one,
+                                const size_t size)
 {
-
-    if (size == 0)
-    {
-        return 1; // error: size cannot be zero
-    }
-
-    if (is_zero_b_uint_arr(num, size))
-    {
-        zero_b_uint_arr(result, size);
-        return 0;
-    }
-
-    b_uint *shift_buffer_left = (b_uint *)malloc(size * sizeof(b_uint));
-    b_uint *shift_buffer_right = (b_uint *)malloc(size * sizeof(b_uint));
-    b_uint *b_uint_one = (b_uint *)malloc(size * sizeof(b_uint));
-#define MAYBE_FREE(v) \
-    do                \
-    {                 \
-        if (v)        \
-        {             \
-            free(v);  \
-        }             \
-    } while (0);
-
-    if (!shift_buffer_left || !shift_buffer_right || !b_uint_one)
-    {
-        MAYBE_FREE(shift_buffer_left);
-        MAYBE_FREE(shift_buffer_right);
-        MAYBE_FREE(b_uint_one);
-        return 1; // error: memory allocation failed
-    }
-
     zero_b_uint_arr(shift_buffer_left, size);
     zero_b_uint_arr(shift_buffer_right, size);
     zero_b_uint_arr(b_uint_one, size);
     b_uint_one[size - 1] = 1;
-
-#define RETURN_AFTER_FREE(v)      \
-    do                            \
-    { /* free here */             \
-        free(shift_buffer_left);  \
-        free(shift_buffer_right); \
-        free(b_uint_one);         \
-        return v;                 \
-    } while (0);
 
 #define SUM_LEFT_RIGHT_AND_MAYBE_PANIC                                                              \
     do                                                                                              \
@@ -107,7 +68,7 @@ int div_10_b_uint_arr(const b_uint *num, b_uint *result, uint8_t *remainder, con
         int carry = add_b_uint_arr(shift_buffer_left, shift_buffer_right, shift_buffer_left, size); \
         if (carry)                                                                                  \
         {                                                                                           \
-            RETURN_AFTER_FREE(1); /* should not happen? but if it does, i don't know what to do  */ \
+            return 1; /* should not happen? but if it does, i don't know what to do  */             \
         }                                                                                           \
     } while (0);
 
@@ -163,26 +124,46 @@ int div_10_b_uint_arr(const b_uint *num, b_uint *result, uint8_t *remainder, con
 
     copy_b_uint_arr(shift_buffer_left, result, size);
     *remainder = rem;
-    RETURN_AFTER_FREE(0);
+    return 0;
 }
 
-int div_10_b_dec(const b_dec num, b_dec *result, uint8_t *remainder)
+int div_10_b_uint_arr(const b_uint *num, b_uint *result, uint8_t *remainder, const size_t size)
 {
-    if (is_zero_b_dec(num))
+
+    if (size == 0)
     {
-        zero(result);
+        return 1; // error: size cannot be zero
+    }
+
+    if (is_zero_b_uint_arr(num, size))
+    {
+        zero_b_uint_arr(result, size);
         return 0;
     }
 
-    b_uint r[CHUNKSIZE];
-    const int error = div_10_b_uint_arr(num.mag, r, remainder, CHUNKSIZE);
-    if (error)
-    {
-        return error; // error in division
-    }
+    b_uint *shift_buffer_left = (b_uint *)malloc(size * sizeof(b_uint));
+    b_uint *shift_buffer_right = (b_uint *)malloc(size * sizeof(b_uint));
+    b_uint *b_uint_one = (b_uint *)malloc(size * sizeof(b_uint));
 
-    result->sign = num.sign;
-    result->prec = num.prec + 1;
-    copy_b_uint_arr(r, result->mag, CHUNKSIZE);
-    return 0;
+#define MAYBE_FREE(v) \
+    do                \
+    {                 \
+        if (v)        \
+        {             \
+            free(v);  \
+        }             \
+    } while (0);
+
+    if (!shift_buffer_left || !shift_buffer_right || !b_uint_one)
+    {
+        MAYBE_FREE(shift_buffer_left);
+        MAYBE_FREE(shift_buffer_right);
+        MAYBE_FREE(b_uint_one);
+        return 1; // error: memory allocation failed
+    }
+    int overflow = div_10_b_uint_arr_no_malloc(num, result, remainder, shift_buffer_left, shift_buffer_right, b_uint_one, size);
+    free(shift_buffer_left);
+    free(shift_buffer_right);
+    free(b_uint_one);
+    return overflow;
 }
