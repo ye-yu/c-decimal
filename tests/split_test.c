594801cabfbf8d50480d107de9eff5d7a8bb28cd
raflie <rafolwen98@gmail.com>
2025-08-11 15:01:58 +0800

    split: Add split at pos 0

--- tests/split_test.c
+++ tests/split_test.c
@@ -12,6 +12,22 @@ void tearDown(void)
 {
 }
 
+void test_split_at_array_0(void)
+{
+    b_uint arr[ARRAYSIZE] = {0, 0b1011011};
+    b_uint first_half[ARRAYSIZE];
+    b_uint last_half[ARRAYSIZE];
+
+    int error = split_arr_at(arr, 0, first_half, last_half, ARRAYSIZE);
+    TEST_ASSERT_EQUAL(0, error);
+
+    b_uint first_half_expected[ARRAYSIZE] = {0, 0b1011011};
+    b_uint last_half_expected[ARRAYSIZE] = {0, 0};
+
+    TEST_ASSERT_EQUAL_INT_ARRAY(first_half_expected, first_half, ARRAYSIZE);
+    TEST_ASSERT_EQUAL_INT_ARRAY(last_half_expected, last_half, ARRAYSIZE);
+}
+
 void test_split_at_array_lt_bit_size(void)
 {
     b_uint arr[ARRAYSIZE] = {0, 0b1011011};
@@ -90,6 +106,7 @@ void test_split_at_array_double_gt_bit_size(void)
 int main()
 {
     UNITY_BEGIN();
+    RUN_TEST(test_split_at_array_0);
     RUN_TEST(test_split_at_array_lt_bit_size);
     RUN_TEST(test_split_at_array_eq_bit_size);
     RUN_TEST(test_split_at_array_gt_bit_size);
