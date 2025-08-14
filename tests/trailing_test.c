c2bb2ed419599d4c2acca544da3ae853c153840d
raflie <rafolwen98@gmail.com>
2025-08-14 10:08:34 +0800

    trailing: add trailing test

--- tests/trailing_test.c
+++ tests/trailing_test.c
@@ -1,29 +1,59 @@
 
 #include "unity.h"
+#include "b_dec.h"
 
-// Function prototypes for the code being tested
-void test_function1(void);
-void test_function2(void);
-
-void setUp(void) {
+void setUp(void)
+{
     // Set up code here
 }
 
-void tearDown(void) {
+void tearDown(void)
+{
     // Tear down code here
 }
 
-void test_function1(void) {
-    TEST_ASSERT_EQUAL(1, 1); // Example assertion
+void test_remove_trailing(void)
+{
+    b_dec a;
+    zero(&a);
+    a.prec = 5;
+    a.mag[CHUNKSIZE - 1] = 0b10100;
+    int result = remove_trailing_zeroes(a.mag, &a.prec, CHUNKSIZE);
+    TEST_ASSERT_FALSE(result);
+    TEST_ASSERT_EQUAL(0b101, a.mag[CHUNKSIZE - 1]);
+    TEST_ASSERT_EQUAL(3, a.prec);
 }
 
-void test_function2(void) {
-    TEST_ASSERT_TRUE(1 == 1); // Example assertion
+void test_remove_trailing_shift(void)
+{
+    b_dec a;
+    zero(&a);
+    a.prec = 5;
+    a.mag[CHUNKSIZE - 2] = 0b1;
+    a.mag[CHUNKSIZE - 1] = 0b10100;
+    int result = remove_trailing_zeroes(a.mag, &a.prec, CHUNKSIZE);
+    TEST_ASSERT_FALSE(result);
+    TEST_ASSERT_EQUAL(0b101 | (~(ALL_ONES >> 1) >> 1), a.mag[CHUNKSIZE - 1]);
+    TEST_ASSERT_EQUAL(3, a.prec);
 }
 
-int main(void) {
+void test_remove_trailing_non_neg_prec(void)
+{
+    b_dec a;
+    zero(&a);
+    a.prec = 5;
+    a.mag[CHUNKSIZE - 1] = 0b101000000;
+    int result = remove_trailing_zeroes(a.mag, &a.prec, CHUNKSIZE);
+    TEST_ASSERT_FALSE(result);
+    TEST_ASSERT_EQUAL(0b1010, a.mag[CHUNKSIZE - 1]);
+    TEST_ASSERT_EQUAL(0, a.prec);
+}
+
+int main(void)
+{
     UNITY_BEGIN();
-    RUN_TEST(test_function1);
-    RUN_TEST(test_function2);
+    RUN_TEST(test_remove_trailing);
+    RUN_TEST(test_remove_trailing_shift);
+    RUN_TEST(test_remove_trailing_non_neg_prec);
     return UNITY_END();
 }
